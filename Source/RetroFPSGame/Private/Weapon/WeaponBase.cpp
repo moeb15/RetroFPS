7d650d49d42e32499d7f9d6d392651a5a15aa1f8
Mujtaba Bader <mujtaba.bader@hotmail.com>
2024-07-05 22:06:37 -0400

    weapon base class

---
+++ Source/RetroFPSGame/Private/Weapon/WeaponBase.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/WeaponBase.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "PaperFlipbookComponent.h"
+#include "Components/PointLightComponent.h"
+
+// Sets default values
+AWeaponBase::AWeaponBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Camera = CreateDefaultSubobject<USceneCaptureComponent2D>("Camera");
+	Camera->SetupAttachment(RootComponent);
+
+	WeaponFlipbook = CreateDefaultSubobject<UPaperFlipbookComponent>("Weapon Flipbook");
+	WeaponFlipbook->SetupAttachment(Camera);
+
+	PointLight = CreateDefaultSubobject<UPointLightComponent>("Point Light");
+	PointLight->SetupAttachment(Camera);
+}
+
+// Called when the game starts or when spawned
+void AWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Camera->ShowOnlyComponent(WeaponFlipbook);
+}
+
+// Called every frame
+void AWeaponBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/RetroFPSGame/Public/Weapon/WeaponBase.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+class USceneCaptureComponent2D;
+class UPaperFlipbookComponent;
+class UPointLightComponent;
+
+UCLASS()
+class RETROFPSGAME_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeaponBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Camera,
+		meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USceneCaptureComponent2D> Camera;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Camera,
+		meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPaperFlipbookComponent> WeaponFlipbook;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Camera,
+		meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPointLightComponent> PointLight;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
