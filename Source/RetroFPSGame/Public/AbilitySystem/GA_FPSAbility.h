8f18ef93eba7cea27f944f1ab7a853e1b34175b2
Mujtaba Bader <mujtaba.bader@hotmail.com>
2024-07-19 06:33:37 -0400

    added gameplay tag for input tag

--- Source/RetroFPSGame/Public/AbilitySystem/GA_FPSAbility.h
+++ Source/RetroFPSGame/Public/AbilitySystem/GA_FPSAbility.h
@@ -18,46 +18,7 @@ UCLASS()
 class RETROFPSGAME_API UGA_FPSAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
-/*
-protected:
-	virtual void ActivateAbility(
-		const FGameplayAbilitySpecHandle Handle,
-		const FGameplayAbilityActorInfo* ActorInfo, 
-		const FGameplayAbilityActivationInfo ActivationInfo, 
-		const FGameplayEventData* TriggerEventData) override;
 
 public:
-	UFUNCTION(BlueprintNativeEvent)
-	void FireWeapon();
-	virtual void FireWeapon_Implementation() = 0;
-
-	UFUNCTION(BlueprintCallable)
-	void WaitFor();
-
-	virtual bool CommitAbility(
-		const FGameplayAbilitySpecHandle Handle,
-		const FGameplayAbilityActorInfo* ActorInfo, 
-		const FGameplayAbilityActivationInfo ActivationInfo, 
-		OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) override;
-
-private:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Gameplay Tags",
-		meta = (AllowPrivateAccess = "true"))
-	FGameplayTag WeaponEventTag;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gameplay Task",
-		meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UAbilityTask_WaitGameplayEvent> GameplayEventWaitTask;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gameplay Tags",
-		meta = (AllowPrivateAccess = "true"))
-	TSubclassOf<UGameplayEffect> WeaponGameplayEffectClass;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gameplay Task",
-		meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UAbilityTask_WaitDelay> GameplayEventWaitDelayTask;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Gameplay Task",
-		meta = (AllowPrivateAccess = "true"))
-	float AbilityCooldown = 1.0f;*/
+	FGameplayTag StartInputTag;
 };
