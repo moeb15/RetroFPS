278108d5c6ebdd29235cd5d317320ca86fdbb2fe
Mujtaba Bader <mujtaba.bader@hotmail.com>
2024-08-15 12:50:40 -0400

    added inithud and getter for overlay widget controller

--- Source/RetroFPSGame/Private/HUD/PlayerHUD.cpp
+++ Source/RetroFPSGame/Private/HUD/PlayerHUD.cpp
@@ -3,3 +3,22 @@
 
 #include "HUD/PlayerHUD.h"
 
+
+void UPlayerHUD::InitHUD(UAbilitySystemComponent* ASC, UAttributeSet* AS)
+{
+	check(OverlayWidgetControllerClass);
+
+	UOverlayWidgetController* WC = GetOverlayWidgetController(ASC, AS);
+	WC->BroadcastInitialValues();
+}
+
+UOverlayWidgetController* UPlayerHUD::GetOverlayWidgetController(UAbilitySystemComponent* ASC, UAttributeSet* AS)
+{
+	if (OverlayWidgetController == nullptr) {
+		OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
+		OverlayWidgetController->ASC = ASC;
+		OverlayWidgetController->AS = AS;
+		OverlayWidgetController->BindCallbacksToDependencies();
+	}
+	return OverlayWidgetController;
+}
--- Source/RetroFPSGame/Public/HUD/PlayerHUD.h
+++ Source/RetroFPSGame/Public/HUD/PlayerHUD.h
@@ -4,8 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "HUD/Controller/OverlayWidgetController.h"
 #include "PlayerHUD.generated.h"
 
+
+class UAbilitySystemComponent;
+class UAttributeSet;
 /**
  * 
  */
@@ -13,5 +17,17 @@ UCLASS()
 class RETROFPSGAME_API UPlayerHUD : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	void InitHUD(UAbilitySystemComponent* ASC, UAttributeSet* AS);
+
+	UOverlayWidgetController* GetOverlayWidgetController(UAbilitySystemComponent* ASC, UAttributeSet* AS);
+
+private:
+	UPROPERTY()
+	TObjectPtr<UOverlayWidgetController> OverlayWidgetController;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Overlay Widget Controller", 
+		meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
 };
